603439a164a8bbf3caede2ce8b4d62b9d8dd1257
Siyu Long <i@longern.com>
2019-07-08 23:08:51 +0800

    Import iconfont

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -19,6 +19,8 @@
   }
 </script>
 
-<style>
-  /* CSS */
+<style lang="scss">
+$material-design-icons-font-directory-path: '~material-design-icons-iconfont/dist/fonts/';
+
+@import '~material-design-icons-iconfont/src/material-design-icons';
 </style>
