307e97bad8741b41f820f4f1a832af4d2a1956e6
Siyu Long <i@longern.com>
2019-07-11 17:17:58 +0800

    Install vue i18n

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,16 +1,23 @@
 import Vue from 'vue'
+import VueI18n from 'vue-i18n'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 
 import App from './App'
 
+Vue.use(VueI18n)
 Vue.use(Vuetify)
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.config.productionTip = false
 
+const i18n = new VueI18n({
+  locale: 'zh-hans'
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  i18n,
   render: h => h(App)
 })
